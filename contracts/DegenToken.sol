e954cbe636bb0fdb1e406d2e77ceed42a8676cbd
psm1020 <140382546+psm1020@users.noreply.github.com>
2024-06-12 18:57:25 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -1,25 +1,23 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
-    constructor(uint initialSupply) ERC20("Degen Token", "DGN") Ownable(msg.sender) {
-        _mint(msg.sender, initialSupply);
+
+    constructor(address initialOwner) ERC20("Degen", "DGN") Ownable(initialOwner) {
+        _mint(initialOwner, 1000 * 10 ** decimals()); // Initial mint for the owner
     }
 
-    event ItemPurchased(address indexed player, uint indexed itemId, string itemName, uint price);
-    event TokensPurchased(address indexed player, uint amount);
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
-    function purchaseTokens(uint amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        
-        _mint(msg.sender, amount);
-
-        emit TokensPurchased(msg.sender, amount);
+    function burn(uint256 burn_amt) public override {
+        require(balanceOf(msg.sender) >= burn_amt, "Insufficient Balance");
+        _burn(msg.sender, burn_amt);
     }
 
     function buyItem(uint itemId) external {
@@ -42,9 +40,9 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
         require(balanceOf(msg.sender) >= price, "Insufficient balance");
 
         _burn(msg.sender, price);
-
         emit ItemPurchased(msg.sender, itemId, itemName, price);
     }
+
     function bonus(uint amount) external {
         require(amount > 0, "Amount must be greater than zero");
 
@@ -53,6 +51,20 @@ contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
         _mint(msg.sender, totalAmount);
     }
+
+    function getItemName(uint itemId) public pure returns (string memory) {
+        if (itemId == 1 || itemId == 2 || itemId == 3) {
+            return "Cycle";
+        } else if (itemId == 4) {
+            return "Bike";
+        } else if (itemId == 5) {
+            return "Car";
+        } else {
+            return "Unknown";
+        }
+    }
+
+    event ItemPurchased(address indexed player, uint indexed itemId, string itemName, uint price);
 
     function getBalance() public view returns (uint256) {
         return balanceOf(msg.sender);
